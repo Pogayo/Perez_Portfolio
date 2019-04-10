855bdbc9950dc4da7f8030d443855d3e020384e5
Pogayo <pogayo17@alustudent.com>
2019-04-10 10:09:09 +0200

    Updates

---
+++ precache-manifest.fcd6346cac880b297d1ea566dbc47fba.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "a29bf1a0cd4a651a84f5db5a619bb492",
+    "url": "/Perez_Portfolio/static/media/email-icon.a29bf1a0.svg"
+  },
+  {
+    "revision": "a3400a99c79ae9024d08",
+    "url": "/Perez_Portfolio/static/css/main.2948d4d2.chunk.css"
+  },
+  {
+    "revision": "137dfd9c8a24ee11853b",
+    "url": "/Perez_Portfolio/static/js/runtime~main.137dfd9c.js"
+  },
+  {
+    "revision": "602bf70720e014a02a76",
+    "url": "/Perez_Portfolio/static/js/2.602bf707.chunk.js"
+  },
+  {
+    "revision": "a141c79af2bcf3e1f42c4927596662ff",
+    "url": "/Perez_Portfolio/static/media/phone-icon.a141c79a.svg"
+  },
+  {
+    "revision": "a3400a99c79ae9024d08",
+    "url": "/Perez_Portfolio/static/js/main.a3400a99.chunk.js"
+  },
+  {
+    "revision": "3b5e36b910f49933fb59ce8ca7a2fd40",
+    "url": "/Perez_Portfolio/static/media/post-icon.3b5e36b9.svg"
+  },
+  {
+    "revision": "7d7068e7a6f4b5a9d208d1e8e0ed9e5b",
+    "url": "/Perez_Portfolio/static/media/code-pic.7d7068e7.jpg"
+  },
+  {
+    "revision": "cf817ef042e1b8e729a2e5e3a1e40dbd",
+    "url": "/Perez_Portfolio/static/media/hult-pitching.cf817ef0.jpg"
+  },
+  {
+    "revision": "6e978adc8c675b30541d78ed07a1a3b3",
+    "url": "/Perez_Portfolio/static/media/Vuga-hackathon-.6e978adc.jfif"
+  },
+  {
+    "revision": "cb9ba661b85bb4876187fc40873adbdd",
+    "url": "/Perez_Portfolio/index.html"
+  }
+];
